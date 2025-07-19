e894416786f45620ef458e9162aa6eaaba23062c
Rihabz1 <mdfoisal552211@gmail.com>
2025-07-19 14:20:43 +0600

    mod8 class3

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,11 @@
+import 'package:demo_app/mod8c3.dart';
 import 'package:flutter/material.dart';
-import 'package:demo_app/mod8.dart';
-import 'package:demo_app/mod8_C2.dart';
-import 'package:demo_app/2nd_class.dart';
 import 'package:demo_app/list_view.dart';
+import 'package:demo_app/stack.dart';
 class Myapp extends StatelessWidget {
   const Myapp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'This is a demo app', home: listV());
+    return MaterialApp(title: 'This is a demo app', home: stk(), debugShowCheckedModeBanner: false);
   }
 }
